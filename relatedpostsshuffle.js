4f976c6a179c27bd37e31139209acb89ac10caa0
sylphid3 <32337759+sylphid3@users.noreply.github.com>
2018-10-12 11:49:39 +0200

    Update relatedpostsshuffle.js

--- relatedpostsshuffle.js
+++ relatedpostsshuffle.js
@@ -96,7 +96,7 @@ function init() {
     return;
   }
 
-  var link = $('<a href="javascript:;">Voir plus darticles</a>');
+  var link = $('<a href="javascript:;">Load more posts</a>');
   link.click(loadMore);
   var img = $('<img src="' + loadingGif + '" style="display: none;">');
 
